02a7e67e8af9e0dcd4ea9e00ab59351e77263bee
piotrMatla <piotrmatla@yahoo.pl>
2024-11-03 16:16:03 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValidationLab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
